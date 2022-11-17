1309807b1c4e1e5b01a101700709951572c385f1
Federico Sierra <federico.sierradodo@cencosud.com>
2022-11-17 13:22:02 -0300

    Before adding files

---
+++ README.md
@@ -0,0 +1,19 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Teslo API
+
+1. Clonar app
+2. ```npm install``
+3. Clonar archivo ```.env.template``` y renombrarlo ```.env```
+4. Setear las variables de entorno en el nuevo ```.env```
+5. Levantar la base de datos
+```
+docker-compose up -d
+```
+6. Levantar ```npm run start:dev```
+7. Ejecutar Seed para insertar datos
+```
+http://localhost:3000/api/seed
+```
---
+++ src/products/products.service.ts
@@ -0,0 +1,185 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PaginationDto } from '../common/dtos/pagination.dto';
+import { DataSource, Repository } from 'typeorm';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { validate as isUUID } from 'uuid';
+import { ProductImage, Product } from './entities';
+
+@Injectable()
+export class ProductsService {
+  private readonly logger = new Logger('ProductsService');
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+
+    @InjectRepository(ProductImage)
+    private readonly productImageRepository: Repository<ProductImage>,
+
+    private readonly dataSource: DataSource,
+  ) {}
+
+  
+/*
+  --> CREATE PRODUCT
+*/
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const { images = [], ...productDetails } = createProductDto;
+
+      const product = this.productRepository.create({
+        ...productDetails,
+        images: images.map((image) =>
+          this.productImageRepository.create({ url: image }),
+        ),
+      });
+
+      await this.productRepository.save(product);
+      return { ...product, images };
+    } catch (error) {
+      this.handleDBExeptions(error);
+    }
+  }
+
+
+/*
+  --> FIND ALL PRODUCTS 
+*/ 
+  async findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+
+    const products = await this.productRepository.find({
+      take: limit,
+      skip: offset,
+      relations: {
+        images: true,
+      },
+    });
+
+    return products.map((product) => ({
+      ...product,
+      images: product.images.map((img) => img.url),
+    }));
+  }
+
+
+/*
+  --> FIND ONE PRODUCT
+*/
+  async findOne(term: string) {
+    let product: Product;
+
+    if (isUUID(term)) {
+      product = await this.productRepository.findOneBy({ id: term });
+    } else {
+      const queryBuilder = this.productRepository.createQueryBuilder('prod');
+      product = await queryBuilder
+        .where('UPPER(title) =:title or slug =:slug', {
+          title: term.toUpperCase(),
+          slug: term,
+        })
+        .leftJoinAndSelect('prod.images', 'prodImages')
+        .getOne();
+    }
+
+    if (!product) throw new NotFoundException('Product not found');
+
+    return product;
+  }
+
+
+/*
+  --> FIND ONE PRODUCT PLAIN
+*/
+  async findOnePlain(term: string) {
+    const { images = [], ...rest } = await this.findOne(term);
+    return {
+      ...rest,
+      images: images.map((image) => image.url),
+    };
+  }
+
+
+/*
+  --> UPDATE PRODUCT
+*/
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const { images, ...toUpdate } = updateProductDto;
+
+    const product = await this.productRepository.preload({
+      id,
+      ...toUpdate,
+    });
+
+    if (!product)
+      throw new NotFoundException(`Product with id: ${id} not found`);
+
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      if (images) {
+        //Si se hace update con images borro las anteriores
+        await queryRunner.manager.delete(ProductImage, { product: { id } });
+
+        //Guardo las nuevas images en e product
+        product.images = images.map((image) =>
+          this.productImageRepository.create({ url: image }),
+        );
+      }
+
+      await queryRunner.manager.save(product);
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
+      return this.findOnePlain(id);
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      await queryRunner.release();
+      this.handleDBExeptions(error);
+    }
+  }
+
+
+/*
+  --> DELETE PRODUCT
+*/
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    await this.productRepository.remove(product);
+    return product;
+  }
+
+
+/*
+  --> DELETE ALL PRODUCTS
+*/
+  async deleteAllProducts() {
+    const query = this.productRepository.createQueryBuilder('product');
+
+    try {
+      return await query.delete().where({}).execute();
+    } catch (error) {
+      this.handleDBExeptions(error);
+    }
+  }
+
+
+/*
+  --> HANDLE ERRORS
+*/
+  private handleDBExeptions(error: any) {
+    if (error.code === '23505') throw new BadRequestException(error.detail);
+
+    this.logger.error(error);
+    throw new InternalServerErrorException('Something went wrong!');
+  }
+}
